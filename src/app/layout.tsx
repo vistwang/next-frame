b53769d32fd7c6cde30ac4378a505c12d468db68
wangyangyang <wangyangyang-015@cpic.com.cn>
2024-11-20 13:55:31 +0800

    feat: lexical demo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,9 +18,45 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full bg-gray-100">
       <body className={`${inter.className} h-full justify-between flex flex-col`}>
-        <header className="bg-blue-400 py-3 px-2 text-white text-right"><Link href="/" className="mr-5">
-      首页
-      </Link>我是头</header>
+        <header className="bg-blue-400 py-3 px-2 text-white text-right">
+          <Link className="mr-5 float-left" href="/">
+          首页
+          </Link>
+          
+        <Link className="mr-5" href="/posts/first-post">
+          Blog Post
+        </Link>
+      
+        <Link className="mr-5" href="/css/use-css">
+          use-css
+        </Link>
+      
+        <Link className="mr-5" href="/products">
+          服务端组件 products
+        </Link>
+      
+        <Link className="mr-5" href="/user">
+          客户端组件 user
+        </Link>
+      
+        <Link className="mr-5" href="/blog">
+          SSG 博客
+        </Link>
+      
+        <Link className="mr-5" href="/openai">
+          openAI
+        </Link>
+      
+        <Link className="mr-5" href="/grok">
+          grok2
+        </Link>
+        <Link className="mr-5" href="/lexical">
+        lexical富文本
+        </Link>
+          <div className="inline r">
+            我是头
+          </div>
+        </header>
         <main className="flex-1">{children}</main>
         <footer className="bg-blue-400 py-3 px-2 text-white text-right">我是尾</footer>
       </body>
